724622977aaa75f5c4d44217331c9b64ca6e3bbf
jasmine.bumbray@my.strayer.edu <jasmine.bumbray@my.strayer.edu>
2023-01-27 15:26:52 -0800

    all def has been added

--- Country.py
+++ Country.py
@@ -2,7 +2,28 @@
 #CIS261
 #Country
 
-print("COMMAND MENU")
-print("list - Show all country")
-print("add - Add a new country")
-print("del - Delete country from list")
+def display_contrylist():
+    print("Country List")
+    print("COMMAND MENU")
+    print("list - Show all country")
+    print("add - Add a new country")
+    print("remove - Delete country from list")
+    print("done - Exit the list")
+
+def list(contry_list):
+    for c, county in enumerate(contry_list, start=1):
+        print(f"{c}, {country}")
+    print()
+
+def add(contry_list):
+    country = input("Enter Country name: ")
+    contry_list.append(country)
+    print(f"{country} has been added. \n")
+
+def remove(contry_list):
+    number = int(input("Number: "))
+    if number < 1 or number > len(contry_list):
+        print("Invalid Country number. \n")
+    else:
+        country = contry_list.pop(number-1)
+        print(f"{country} has been delete. /n")
