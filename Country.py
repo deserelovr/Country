15fe6a43a6cba2e4a5578e5ad63ee3f95d55a2fa
jasmine.bumbray@my.strayer.edu <jasmine.bumbray@my.strayer.edu>
2023-01-27 17:18:33 -0800

    if/else statements added

--- Country.py
+++ Country.py
@@ -5,25 +5,71 @@
 def display_contrylist():
     print("Country List")
     print("COMMAND MENU")
-    print("list - Show all country")
+    print("show - Show all country")
     print("add - Add a new country")
     print("remove - Delete country from list")
     print("done - Exit the list")
 
-def list(contry_list):
-    for c, county in enumerate(contry_list, start=1):
-        print(f"{c}, {country}")
-    print()
+def display_codes(countries):
+    codes = list(countries.keys())
+    codes.sort()
+    codes_line = "Country codes: "
+    for code in codes:
+        codes_line += code + " "
+    print(codes_line)
 
-def add(contry_list):
-    country = input("Enter Country name: ")
-    contry_list.append(country)
-    print(f"{country} has been added. \n")
-
-def remove(contry_list):
-    number = int(input("Number: "))
-    if number < 1 or number > len(contry_list):
-        print("Invalid Country number. \n")
+def view(countries):
+    display_codes(countries)
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries[code]
+        print(f"Country name: {name}.\n")
     else:
-        country = contry_list.pop(number-1)
-        print(f"{country} has been delete. /n")
+        print("There is no country with that code.\n")
+
+def add(countries):
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries[code]
+        print(f"{name} is already using this code.\n")
+    else:
+        name = input("Enter country name: ")
+        name = name.title()
+        countries[code] = name
+        print(f"{name} was added.\n")
+
+def delete(countries):
+    code = input("Enter country code: ")
+    code = code.upper()
+    if code in countries:
+        name = countries.pop(code)
+        print(f"{name} was deleted.\n")
+    else:
+        print("There is no country with that code.\n")
+
+def main():
+    countries = {"CA": "Canada",
+                 "JA": "Japan",
+                 "DK": "Denmark"}
+    
+    display_contrylist()
+    while True:        
+        command = input("Command: ")
+        command = command.lower()
+        if command == "show":
+            view(countries)   
+        elif command == "add":
+            add(countries)
+        elif command == "remove":
+            delete(countries)  
+        elif command == "done":
+            print("See Ya!")
+            break
+        else:
+            print("Not a valid command. Please try again.\n")
+
+
+if __name__ == "__main__":
+    main()
